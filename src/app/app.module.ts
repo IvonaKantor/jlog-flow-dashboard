e1f02005331f2c1ed21b50e40eff8ecb3441468c
ivona <ik87056@student.uwb.edu.pl>
2025-12-30 12:55:09 +0100

    added the ts module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,2 +1,18 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component'
+import {WjCoreModule} from '@mescius/wijmo.angular2.core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    WjCoreModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
