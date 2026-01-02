1acdce185aae1d34bf1d853e45fcd2902c96d419
ivona <ik87056@student.uwb.edu.pl>
2026-01-02 17:26:06 +0100

    auth module

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,1 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthLayout} from '../../layout/auth-layout.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    AuthLayout
+  ],
+  templateUrl: './login.component.html',
+})
+
+export class LoginComponent {
+}
