5e510d805ca1dcf6ffa05c9671f078ad01514a2c
ivona <ik87056@student.uwb.edu.pl>
2026-01-25 23:26:50 +0100

    app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { authGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
