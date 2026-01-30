fd2b1821bd7dd63fcb390523f82727de3eb6171c
ivona <ik87056@student.uwb.edu.pl>
2026-01-30 22:24:35 +0100

    app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
-import {Component} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterModule
-  ],
-  template: `./app.component.html`,
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`
 })
-
-export class AppComponent {
-  title = 'Jlog Flow Dashboard';
-}
+export class AppComponent {}
