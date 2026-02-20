47bd2ea077d328fd120787c2b4ee6b8ad1a96e2d
ivona <ik87056@student.uwb.edu.pl>
2026-02-20 16:25:18 +0100

    keycloak init

---
+++ src/app/core/auth/keycloak-init.ts
@@ -0,0 +1,20 @@
+import {KeycloakService} from 'keycloak-angular';
+
+export function initKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'jlog-flow-realm',
+        clientId: 'angular-dash'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      enableBearerInterceptor: true,
+      bearerPrefix: 'Bearer',
+      bearerExcludedUrls: ['/assets']
+    });
+}
