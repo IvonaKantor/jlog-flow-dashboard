9c3dc55c01161d7b87a7bdd273251e4d7d65e767
ivona <ik87056@student.uwb.edu.pl>
2026-03-06 19:48:41 +0100

    main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LogService} from './services/log.service';
+import {Log} from './models/log.model';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [
+    DatePipe
+  ],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-dashboard';
+export class AppComponent implements OnInit {
+  logs: Log[] = [];
+  loading = true;
+  error: string | null = null;
+
+  constructor(private logService: LogService) {
+  }
+
+  ngOnInit() {
+    this.loadLogs();
+  }
+
+  loadLogs() {
+    this.loading = true;
+    this.logService.getLogs().subscribe({
+      next: (data) => {
+        this.logs = data;
+        this.loading = false;
+        console.log('get logs:', data);
+      },
+      error: (err) => {
+        this.error = 'cannt connect to the backend: ' + err.message;
+        this.loading = false;
+        console.error('error:', err);
+      }
+    });
+  }
 }
