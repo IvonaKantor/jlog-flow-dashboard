c17271216ac72a78c97b339527f6ef639815bd96
ivona <ik87056@student.uwb.edu.pl>
2026-03-06 19:49:54 +0100

    imported app component and browser module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,18 @@
 import { NgModule } from '@angular/core';
-@NgModule({
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppComponent,
+  ],
+  providers: []
 })
 export class AppModule { }
