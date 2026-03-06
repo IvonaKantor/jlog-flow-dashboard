7cb3e8caacf2416ba8f53aad2b556a9f92f05d75
ivona <ik87056@student.uwb.edu.pl>
2026-03-06 17:17:22 +0100

    get log service

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -7,31 +7,29 @@ import {Log} from '../models/log.model';
   providedIn: 'root'
 })
 export class LogService {
-  private apiUrl = '/api/logs';
+  private apiUrl = 'http://localhost:8080/api/logs';
 
   constructor(private http: HttpClient) {
   }
 
-  getLogs(filters?: any): Observable<Log[]> {
+  getLogs(page?: number, size?: number): Observable<Log[]> {
     let params = new HttpParams();
+    if (page !== undefined) params = params.set('page', page.toString());
+    if (size !== undefined) params = params.set('size', size.toString());
 
-    if (filters) {
-      Object.keys(filters).forEach(key => {
-        const value = filters[key];
-        if (value !== null && value !== undefined && value !== '') {
-          params = params.set(key, value.toString());
-        }
-      });
-    }
-
-    return this.http.get<Log[]>(this.apiUrl, {params});
+    return this.http.get<Log[]>(`${this.apiUrl}/logs`, { params });
   }
 
-  getLogById(id: number): Observable<Log> {
-    return this.http.get<Log>(`${this.apiUrl}/${id}`);
+
+  getLogById(id: string): Observable<Log> {
+    return this.http.get<Log>(`${this.apiUrl}/logs/${id}`);
   }
 
   getServices(): Observable<string[]> {
-    return this.http.get<string[]>(`/api/services`);
+    return this.http.get<string[]>(`${this.apiUrl}/logs/services`);
+  }
+
+  getHosts(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiUrl}/logs/hosts`);
   }
 }
