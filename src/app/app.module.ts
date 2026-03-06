7d09c503b7a27113960f1b4957e2f3389da5aa5b
ivona <ik87056@student.uwb.edu.pl>
2026-03-06 18:44:10 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTooltipModule } from '@angular/material/tooltip';
-
-import { AppComponent } from './app.component';
-import { LogListComponent } from './components/log-list/log-list.component';
-
 @NgModule({
-  declarations: [
 
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatButtonModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-    MatTooltipModule,
-    AppComponent,
-    LogListComponent
-  ],
-  providers: []
 })
 export class AppModule { }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -7,7 +7,7 @@ import {Log} from '../models/log.model';
   providedIn: 'root'
 })
 export class LogService {
-  private apiUrl = 'http://localhost:8080/api/logs';
+  private apiUrl = 'http://localhost:8080/api/log';
 
   constructor(private http: HttpClient) {
   }
