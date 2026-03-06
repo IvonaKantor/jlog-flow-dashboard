6702af4d90f6a8b68bf6869a9f5a2c1e9edc52d8
ivona <ik87056@student.uwb.edu.pl>
2026-03-06 17:46:08 +0100

    added the log component

--- src/app/components/log-list/log-list.component.ts
+++ src/app/components/log-list/log-list.component.ts
@@ -1,146 +1,108 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
-import { Subject, takeUntil, debounceTime, distinctUntilChanged } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { LogService } from '../../services/log.service';
 import { Log } from '../../models/log.model';
-import {MatError, MatFormField, MatLabel} from '@angular/material/input';
-import {MatOption, MatSelect} from '@angular/material/select';
-import {MatProgressSpinner} from '@angular/material/progress-spinner';
-import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
-import {MatTable} from '@angular/material/table';
-import {MatIcon} from '@angular/material/icon';
-import {MatPaginator} from '@angular/material/paginator';
 import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-log-list',
-  templateUrl: './log-list.component.html',
+  template: `
+    <div class="log-container">
+      <table class="log-table">
+        <thead>
+        <tr>
+          <th>Timestamp</th>
+          <th>Level</th>
+          <th>Service</th>
+          <th>Message</th>
+          <th>Host</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr *ngFor="let log of logs"
+            [class.error]="log.level === 'ERROR'"
+            [class.warn]="log.level === 'WARN'"
+            (click)="selectLog(log)">
+          <td>{{ log.timestamp }}</td>
+          <td><span class="badge" [ngClass]="log.level">{{ log.level }}</span></td>
+          <td>{{ log.serviceName }}</td>
+          <td>{{ log.message }}</td>
+          <td>{{ log.hostName }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  `,
   imports: [
-    ReactiveFormsModule,
-    MatFormField,
-    MatSelect,
-    MatOption,
-    MatLabel,
-    MatError,
-    MatProgressSpinner,
-    MatDatepicker,
-    MatDatepickerToggle,
-    MatDatepickerInput,
-    MatTable,
-    MatIcon,
-    MatPaginator,
     NgClass
   ],
-  styleUrls: ['./log-list.component.scss']
+  styles: [`
+    .log-table {
+      width: 100%;
+      border-collapse: collapse;
+    }
+
+    .log-table tr {
+      cursor: pointer;
+    }
+
+    .log-table tr:hover {
+      background-color: #f5f5f5;
+    }
+
+    .error {
+      background-color: #ffebee;
+    }
+
+    .warn {
+      background-color: #fff3e0;
+    }
+
+    .badge {
+      padding: 3px 8px;
+      border-radius: 4px;
+      font-weight: bold;
+    }
+
+    .badge.INFO {
+      background-color: #2196f3;
+      color: white;
+    }
+
+    .badge.WARN {
+      background-color: #ff9800;
+      color: white;
+    }
+
+    .badge.ERROR {
+      background-color: #f44336;
+      color: white;
+    }
+  `]
 })
-export class LogListComponent implements OnInit, OnDestroy {
+export class LogListComponent implements OnInit {
   logs: Log[] = [];
-  loading = false;
-  error: string | null = null;
+  selectedLog: Log | null = null;
+  currentPage = 0;
+  totalLogs = 0;
 
-  filterForm: FormGroup;
+  constructor(private logService: LogService) {}
 
-  services: string[] = [];
-
-  logLevels = ['INFO', 'WARN', 'ERROR', 'DEBUG'];
-
-  private destroy$ = new Subject<void>();
-
-  constructor(
-    private logService: LogService,
-    private fb: FormBuilder
-  ) {
-    this.filterForm = this.fb.group({
-      serviceName: [''],
-      level: [''],
-      searchText: [''],
-      fromDate: [''],
-      toDate: ['']
-    });
-  }
-
-  ngOnInit(): void {
-    this.loadServices();
+  ngOnInit() {
     this.loadLogs();
-
-    this.filterForm.valueChanges
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged(),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(() => {
-        this.loadLogs();
-      });
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  loadServices(): void {
-    this.logService.getServices().subscribe({
-      next: (services) => {
-        this.services = services;
-      },
-      error: (err) => {
-        console.error('Service loading error', err);
-      }
+  loadLogs() {
+    this.logService.getLogs(this.currentPage, 50).subscribe(logs => {
+      this.logs = logs;
     });
   }
 
-  loadLogs(): void {
-    this.loading = true;
-    this.error = null;
-
-    const filters = this.prepareFilters();
-
-    this.logService.getLogs(filters).subscribe({
-      next: (data) => {
-        this.logs = data;
-        this.loading = false;
-      },
-      error: (err) => {
-        this.error = 'Unable to load logs';
-        this.loading = false;
-        console.error('Log retrieval error', err);
-      }
-    });
+  selectLog(log: Log) {
+    this.selectedLog = log;
   }
 
-  prepareFilters(): any {
-    const formValue = this.filterForm.value;
-    const filters: any = {};
-
-    if (formValue.serviceName) {
-      filters.service = formValue.serviceName;
-    }
-
-    if (formValue.level) {
-      filters.level = formValue.level;
-    }
-
-    if (formValue.searchText) {
-      filters.search = formValue.searchText;
-    }
-
-    if (formValue.fromDate) {
-      filters.from = new Date(formValue.fromDate).toISOString();
-    }
-
-    if (formValue.toDate) {
-      filters.to = new Date(formValue.toDate).toISOString();
-    }
-
-    return filters;
-  }
-
-  clearFilters(): void {
-    this.filterForm.reset();
-  }
-
-  hasException(log: Log): boolean {
-    return !!log.exception;
+  loadPage(page: number) {
+    this.currentPage = page;
+    this.loadLogs();
   }
 }
