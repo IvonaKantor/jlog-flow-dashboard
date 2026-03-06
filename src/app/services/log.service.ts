bba8932d6ad194c57b87b4bb4b59ffe7281dc75d
ivona <ik87056@student.uwb.edu.pl>
2026-03-06 19:07:57 +0100

    the log service

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,35 +1,17 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Log} from '../models/log.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Log } from '../models/log.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
-  private apiUrl = 'http://localhost:8080/api/log';
+  private apiUrl = 'http://localhost:8080/v1/log';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
-  getLogs(page?: number, size?: number): Observable<Log[]> {
-    let params = new HttpParams();
-    if (page !== undefined) params = params.set('page', page.toString());
-    if (size !== undefined) params = params.set('size', size.toString());
-
-    return this.http.get<Log[]>(`${this.apiUrl}/logs`, { params });
-  }
-
-
-  getLogById(id: string): Observable<Log> {
-    return this.http.get<Log>(`${this.apiUrl}/logs/${id}`);
-  }
-
-  getServices(): Observable<string[]> {
-    return this.http.get<string[]>(`${this.apiUrl}/logs/services`);
-  }
-
-  getHosts(): Observable<string[]> {
-    return this.http.get<string[]>(`${this.apiUrl}/logs/hosts`);
+  getLogs(): Observable<Log[]> {
+    return this.http.get<Log[]>(this.apiUrl);
   }
 }
