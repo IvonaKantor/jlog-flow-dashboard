4c38e72d3b50c0032b849f7ada2a73f97600fb76
ivona <ik87056@student.uwb.edu.pl>
2026-03-06 16:04:10 +0100

    changed class location

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Log} from '../../models/log.model';
+import {Log} from '../models/log.model';
 
 @Injectable({
   providedIn: 'root'
