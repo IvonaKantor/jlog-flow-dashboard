4b6a9982183182c36f4dc8fd8e2f3cd9117f0228
ivona <ik87056@student.uwb.edu.pl>
2026-03-06 20:37:35 +0100

    imports to the main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch((err: any) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule)
+  ]
+}).catch(err => console.error(err));
