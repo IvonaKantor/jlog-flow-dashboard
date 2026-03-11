007baa0f0f37265efa3203154f511ba0d2d11d07
ivona <ik87056@student.uwb.edu.pl>
2026-03-12 00:45:48 +0100

    main component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {LogService} from './services/log.service';
-import {Log} from './models/log.model';
-import {DatePipe} from '@angular/common';
+import { Component } from '@angular/core';
+import { LogListComponent } from './log-list/log-list.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  imports: [
-    DatePipe
-  ],
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [LogListComponent],
+  template: `
+    <main>
+      <app-log-list></app-log-list>
+    </main>
+  `,
+  styles: [`
+    main {
+      min-height: 100vh;
+      background: #f5f5f5;
+    }
+  `]
 })
-export class AppComponent implements OnInit {
-  logs: Log[] = [];
-  loading = true;
-  error: string | null = null;
-
-  constructor(private logService: LogService) {
-  }
-
-  ngOnInit() {
-    this.loadLogs();
-  }
-
-  loadLogs() {
-    this.loading = true;
-    this.logService.getLogs().subscribe({
-      next: (data) => {
-        this.logs = data;
-        this.loading = false;
-        console.log('get logs:', data);
-      },
-      error: (err) => {
-        this.error = 'cannt connect to the backend: ' + err.message;
-        this.loading = false;
-        console.error('error:', err);
-      }
-    });
-  }
+export class AppComponent {
+  title = 'jlog-flow-dashboard';
 }
